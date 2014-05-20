601566ad7e7b9527b2e911f0d23f5fe51f4277d4
RubenKelevra <cyrond@gmail.com>
2014-05-20 19:04:04 +0200

    Update update_deployFastd.sh

--- update_deployFastd.sh
+++ update_deployFastd.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 mv deployFastd.sh deployFastd.sh.bak
+mv update_deployFastd.sh update_deployFastd.sh.bak
 
 wget https://raw.githubusercontent.com/FF-NRW/DeployScript/master/deployFastd.sh
+wget https://raw.githubusercontent.com/FF-NRW/DeployScript/master/update_deployFastd.sh
+
+chmod +x deployFastd.sh
+chmod +x update_deployFastd.sh
